e3bf679da43e6f423d946698ff8fd4083f7a159f
M. Fatih CIRIT <mfcirit@gmail.com>
2017-06-09 19:10:50 +0300

    no new(Student)

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 #include "University.h"
 
 int main() {
-    auto student1 = new(Student);
-    student1->setName("Fatih Cirit");
-    student1->setGender(Student::Male);
-    student1->setDate(1994, 4, 26);
-    student1->setPhoneNum(5511421052);
-    student1->setDepartmentName("Department of Mechatronics");
-    student1->setFacultyName("Faculity of Mechanics");
+    auto student1 = Student();
+    student1.setName("Fatih Cirit");
+    student1.setGender(Student::Male);
+    student1.setDate(1994, 4, 26);
+    student1.setPhoneNum(5511421052);
+    student1.setDepartmentName("Department of Mechatronics");
+    student1.setFacultyName("Faculity of Mechanics");
 
-    std::cout << "Name: " << student1->getName() << std::endl;
-    std::cout << "Gender: " << student1->getGender() << std::endl;
-    std::cout << "ID: " << student1->getId() << std::endl;
-    std::cout << "Date: " << student1->getDate() << std::endl;
-    std::cout << "Phone: " << student1->getPhoneNum() << std::endl;
-    std::cout << "Department: " << student1->getDepartmentName() << std::endl;
-    std::cout << "Faculity: " << student1->getFacultyName() << std::endl;
+    std::cout << "Name: " << student1.getName() << std::endl;
+    std::cout << "Gender: " << student1.getGender() << std::endl;
+    std::cout << "ID: " << student1.getId() << std::endl;
+    std::cout << "Date: " << student1.getDate() << std::endl;
+    std::cout << "Phone: " << student1.getPhoneNum() << std::endl;
+    std::cout << "Department: " << student1.getDepartmentName() << std::endl;
+    std::cout << "Faculity: " << student1.getFacultyName() << std::endl;
     auto uni = University();
     uni.WelcomeScreen();
 
